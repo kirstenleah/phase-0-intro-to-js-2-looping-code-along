67c367842f2cabb0ba75b63b09cf8532912c6ac1
Kirsten <kirsten.staggs@gmail.com>
2022-04-07 11:43:37 -0600

    commit tree e6e7444831c99bf924c9047cfe33334d2f51eb88

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+ 
+function writeCards(cards, event) {
+    let thankYouCards = []
+     for (let i = 0; i < cards.length; i++) {
+         thankYouCards.push( `Thank you, ${cards[i]}, for the wonderful surprise gift!` );
+     }
+     return thankYouCards;
+    }
+
+
+
+const startingNumber = 11
+
+function countDown(startingNumber) {
+   while (startingNumber > 0) {
+       console.log(startingNumber);
+       startingNumber -= 1;
+   }
+   console.log(startingNumber);
+}
